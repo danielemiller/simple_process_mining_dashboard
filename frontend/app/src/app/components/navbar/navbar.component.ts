28ea9e52b67a2fe9dc118a1a49738934bd50291a
Daniel Miller <danielmiller@Daniels-MBP.lan>
2023-12-07 11:04:46 -0500

    Implemented dynamic navbar based on user authentication status
    Updated NavbarComponent to use AuthService for login check
    
    Added conditional links in navbar for logged-in and logged-out users
    
    Implemented logout method with redirection to login page

--- frontend/app/src/app/components/navbar/navbar.component.ts
+++ frontend/app/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../services/auth-service.service';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(public authService: AuthService, private router: Router) {}
 
+  logout() {
+    // Logic for logging out (e.g., removing the token)
+    localStorage.removeItem('access_token');
+    this.router.navigate(['/login']); // Redirect to login page
+  }
 }
